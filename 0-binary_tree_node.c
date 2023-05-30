0a98fb2513f19980e298ee80105ff153d47e4e48
Webnai <wilbertboadzo144@gmail.com>
2023-05-30 03:46:40 -0700

    binary tree files

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,15 +10,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *node;
-node = malloc(sizeof(binary_tree_t));
-if (node == NULL)
-return (NULL);
+	binary_tree_t *new;
 
-node->n = value;
-node->parent = parent;
-node->left = NULL;
-node->right = NULL;
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
 
-return (node);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
 }
